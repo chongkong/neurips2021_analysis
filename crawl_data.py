a4159dc5dd11d9c5a3d041ee3fb69c313a04669a
Jongbin Park <chongkong94@gmail.com>
2022-04-09 13:57:27 +0900

    Incremental crawling to track progress.

--- crawl_data.py
+++ crawl_data.py
@@ -1,38 +1,41 @@
+import sys
+
 import requests
 from bs4 import BeautifulSoup
-import pickle
 
 page = requests.get("https://nips.cc/Conferences/2021/AcceptedPapersInitial")
 soup = BeautifulSoup(page.text, "html.parser")
 
-url_id = []
+pp_ids = []
 for link in soup.find_all("div", {"class": "maincard narrower poster"}):
-    url_id.append(link.get("id").split("_")[1])
+    pp_ids.append(link.get("id").split("_")[1])
 
 pp_names = []
 pp_authors = []
-for id in url_id:
+
+for i, pp_id in enumerate(pp_ids, start=1):
     pp_soup = BeautifulSoup(
-        requests.get("https://nips.cc/Conferences/2021/Schedule?showEvent=" + id).text, "html.parser"
+        requests.get(f"https://nips.cc/Conferences/2021/Schedule?showEvent={pp_id}").text, "html.parser"
     )
-    pp_name = pp_soup.find("div", {"class": "maincardBody"})
-    pp_names.append(pp_name.get_text())
+    pp_name = pp_soup.find("div", {"class": "maincardBody"}).get_text()
+    pp_names.append(pp_name)
     authors = []
 
     for author in pp_soup.find_all("button", {"class": "btn btn-default"}):
         at_id = author.get("onclick")
         at_id = at_id[at_id.find("('") + 2 : at_id.find("')")]
         at_soup = BeautifulSoup(
-            requests.get("https://neurips.cc/Conferences/2021/Schedule?showSpeaker=" + at_id).text, "html.parser"
+            requests.get(f"https://neurips.cc/Conferences/2021/Schedule?showSpeaker={at_id}").text, "html.parser"
         )
         at_name = at_soup.find("h3").get_text()
         ins_name = at_soup.find("h4").get_text()
-        authors.append(at_name + " (" + ins_name + ")")
+        authors.append(f"{at_name} ({ins_name})")
 
     pp_authors.append(authors)
+    print(f"Processing {i}/{len(pp_ids)}", file=sys.stderr)
 
-with open("pp_names.txt", "wb") as fp:  # Pickling
-    pickle.dump(pp_names, fp)
+    with open("pp_names.txt", "a") as f:
+        print(pp_name, file=f)
 
-with open("pp_authors.txt", "wb") as fp:  # Pickling
-    pickle.dump(pp_authors, fp)
+    with open("pp_authors.txt", "a") as f:
+        print('\n'.join(authors), file=f)
